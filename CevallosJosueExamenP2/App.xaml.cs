9c4b2151a3dce5b36f1e4f87d1bcc262a2347d6b
laboratorio <laboratorio@UPOAULA10216>
2024-12-02 07:20:17 -0500

    Agregar archivos de proyecto.

---
+++ CevallosJosueExamenP2/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace CevallosJosueExamenP2
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ CevallosJosueExamenP2/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace CevallosJosueExamenP2
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
